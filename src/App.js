3a58da3590564a4af7df4d09e1d5211b115bd1b6
plDilipkumar <119333264+plDilipkumar@users.noreply.github.com>
2025-08-09 09:40:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import InvoiceList from './InvoiceList';
 import InvoiceForm from './InvoiceForm';
+import ClientForm from './ClientForm';
+import ClientList from './ClientList';
 
 function App() {
   return (
@@ -11,9 +13,13 @@ function App() {
         <Navbar />
         <main className="container mx-auto mt-6 px-4">
           <Routes>
+            {/* Invoice Routes */}
             <Route path="/" element={<InvoiceList />} />
-            <Route path="/add" element={<InvoiceForm />} />
-            {/* You can add edit route later if needed */}
+            <Route path="/add-invoice" element={<InvoiceForm />} />
+
+            {/* Client Routes */}
+            <Route path="/clients" element={<ClientList />} />
+            <Route path="/add-client" element={<ClientForm />} />
           </Routes>
         </main>
       </div>
